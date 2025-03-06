8ceba1a73d463329362a03f3914a2f60f65a94ce
Joathan <joathanmareto@gmail.com>
2025-03-06 07:42:28 -0400

    eslint e prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import Timer from './components/Timer/Timer.jsx';
-import {Settings} from 'lucide-react';
+import Timer from "./components/Timer/Timer.jsx";
+import { Settings, X } from "lucide-react";
 
 const initialSettings = {
   time: {
     pomodoro: 25,
     shortBreak: 5,
-    longBreak: 15
-  }
-}
+    longBreak: 15,
+  },
+};
 
 function App() {
   return (
     <>
-      <div className='flex justify-center items-center h-screen relative'>
-        <button className='btn btn-link absolute top-0 right-0'>
-          <Settings/>
+      <div className="flex justify-center items-center h-screen relative">
+        <button
+          className="btn btn-link absolute top-0 right-0"
+          onClick={() => document.getElementById("settings_modal").showModal()}
+        >
+          <Settings />
         </button>
-        <Timer initialSettings={initialSettings}/>
+        <dialog id="settings_modal" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                <X />
+              </button>
+            </form>
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">Press ESC key or click on X button to close</p>
+          </div>
+        </dialog>
+        <Timer initialSettings={initialSettings} />
       </div>
     </>
   );
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,7 +1,7 @@
-import {useEffect, useState} from 'react';
-import {Play, Pause, RotateCcw} from 'lucide-react'
+import { useEffect, useState } from "react";
+import { Play, Pause, RotateCcw } from "lucide-react";
 
-function Timer({initialSettings}) {
+function Timer({ initialSettings }) {
   const [time, setTime] = useState(initialSettings.time.pomodoro * 60);
   const [isActive, setIsActive] = useState(false);
 
@@ -36,25 +36,33 @@ function Timer({initialSettings}) {
   function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
   }
 
   return (
-    <div className='card w-128 bg-base-300 card-xl shadow'>
-      <div className='card-body space-y-10'>
-        <h1 className='text-center text-3xl font-medium'>Pomodoro</h1>
-        <h2 className='text-center text-5xl font-bold'>{formatTime(time)}</h2>
-        <div className='card-actions justify-center'>
-          <button className='btn btn-secondary' onClick={startTimer} disabled={isActive}>
-            <Play/>
+    <div className="card w-128 bg-base-300 card-xl shadow">
+      <div className="card-body space-y-10">
+        <h1 className="text-center text-3xl font-medium">Pomodoro</h1>
+        <h2 className="text-center text-5xl font-bold">{formatTime(time)}</h2>
+        <div className="card-actions justify-center">
+          <button
+            className="btn btn-secondary"
+            onClick={startTimer}
+            disabled={isActive}
+          >
+            <Play />
             Iniciar
           </button>
-          <button className='btn btn-error' onClick={pauseTimer} disabled={!isActive}>
-            <Pause/>
+          <button
+            className="btn btn-error"
+            onClick={pauseTimer}
+            disabled={!isActive}
+          >
+            <Pause />
             Pausar
           </button>
-          <button className='btn btn-primary' onClick={resetTimer}>
-            <RotateCcw/>
+          <button className="btn btn-primary" onClick={resetTimer}>
+            <RotateCcw />
             Reiniciar
           </button>
         </div>
